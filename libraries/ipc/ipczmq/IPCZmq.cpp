899d4bc43c7688880efdd23148b0e3f5b42754e0
Lorenzo Giuliani <l89giuliani@gmail.com>
2026-05-27 11:12:26 -0400

    ipc choice at compile time

---
+++ libraries/ipc/ipczmq/IPCZmq.cpp
@@ -0,0 +1,11 @@
+/* @file IPCZmq.cpp
+*  @brief
+*  @author Lorenzo Giuliani
+*  @date 27/05/26
+*  @note
+*/
+//
+// Created by osboxes on 27/05/26.
+//
+
+#include "IPCZmq.h"
---
+++ libraries/ipc/ipczmq/IPCZmq.h
@@ -0,0 +1,16 @@
+//
+// Created by osboxes on 27/05/26.
+//
+
+#ifndef LG_ALARM_CENTRAL_IPCZMQ_H
+#define LG_ALARM_CENTRAL_IPCZMQ_H
+
+
+class IPCZmq {
+private:
+public:
+    static void test(){}
+};
+
+
+#endif //LG_ALARM_CENTRAL_IPCZMQ_H
